a6b5ab16b06e088c7e2ded9973ac1acb3342c669
grantrudow <webdev.grant@gmail.com>
2020-09-04 16:29:23 -0500

    finished implementing all v1 features, now conducting security review

---
+++ controllers/register.js
@@ -0,0 +1,36 @@
+const handleRegister = (req, res, db, bcrypt) => {
+	const { email, name, password } = req.body;
+	// Store hash in your password DB.
+	const salt = bcrypt.genSaltSync(10);
+	const hash = bcrypt.hashSync(password, salt);
+
+	//Create a transaction when you need to do more than 2 things at once
+		db.transaction(trx => {
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+					.returning('*')
+					.insert({
+						email: loginEmail[0],
+						name: name,
+						joined: new Date()
+				})
+				.then(user => {
+					res.json(user[0]);
+				})
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+		})
+		// Don't want to send err back to the user for security
+		.catch(err => res.status(400).json('Unable to register'))
+}
+
+module.exports = {
+	handleRegister: handleRegister
+};
--- server.js
+++ server.js
@@ -2,107 +2,42 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 //Must be installed to link frontend to backend
 const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'admin',
+		database: 'smartbrain'
+	}
+});
 
 const app = express();
 //Allows you to read json values (needed in every project)
 app.use(express.json());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	],
-	login: [
-		{
-			id: '987',
-			hash: '',
-			email: 'john@gmail.com'
-		}
-	]
-}
 
 app.use(cors());
 
 app.get('/', (req, res) => {
+	
 	res.json(database.users);
 })
 
-app.post('/signin', (req, res)=> {
-	// Load hash from your password DB.
-	bcrypt.compare("apples", '$2a$10$W2bmwBTPlFubg4zE/k3HEuO8khymQ6.OXEQOHfIJ1oV3/wSAIxEh.', function(err, res) {
-		// res === true
-		console.log('first guess', res)
-	});
-	bcrypt.compare("not_bacon", '$2a$10$W2bmwBTPlFubg4zE/k3HEuO8khymQ6.OXEQOHfIJ1oV3/wSAIxEh.', function(err, res) {
-		// res === false
-		console.log('second guess', res)
-	});
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
 
-	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json('Error logging in')
-	}
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt) } );
 
-app.post('/register', (req, res) => {
-	const { email, name, password } = req.body;
-	const salt = bcrypt.genSaltSync(10);
-	const hash = bcrypt.hashSync("B4c0/\/", salt);
-	console.log(hash);
-	// Store hash in your password DB.
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		entries: 0,
-		joined: new Date()
-	})
-	res.json(database.users[database.users.length-1])
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
 
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	})
-	if (!found) {
-		res.status(404).json('No such user')
-	}
-})
-
-app.put('/image', (req, res) => {
-	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	})
-	if (!found) {
-		res.status(404).json('No such user')
-	}
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 
 
 
