4aeab9746dbec22f0d9ca6d6524f86f30d778110
grantrudow <webdev.grant@gmail.com>
2020-09-05 09:07:35 -0500

    Adding port

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
 // var hash = bcrypt.hashSync('bacon', 8);
 
-app.listen(3000, () => {
-	console.log('App is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`App is running on port ${process.env.PORT}`)
 });
 
 
