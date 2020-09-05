3ac48d95d2ec2fddc8a1b52142c029ee4b5e012b
grantrudow <webdev.grant@gmail.com>
2020-09-05 09:45:33 -0500

    adding ssl

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-slippery-24776',
-		user: 'postgres',
-		password: 'admin',
-		database: 'smartbrain'
+		host: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
