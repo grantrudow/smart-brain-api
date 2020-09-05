f9d93622013de7b35f61db315610cb75001c29e1
grantrudow <webdev.grant@gmail.com>
2020-09-05 09:14:06 -0500

    fixed homepage response

--- server.js
+++ server.js
@@ -26,10 +26,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-	
-	res.json(database.users);
-})
+app.get('/', (req, res) => {res.send('It is working!')});
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
 
