1edcd87a501a1fa52fb2e0e75d4da65b2bb06f4f
grantrudow <webdev.grant@gmail.com>
2020-09-05 08:51:11 -0500

    pre-deploy commit

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, db, bcrypt) => {
 	const { email, name, password } = req.body;
+	if (!email || !name || !password) {
+		return res.status(400).json('Incorrect form submission')
+	}
 	// Store hash in your password DB.
 	const salt = bcrypt.genSaltSync(10);
 	const hash = bcrypt.hashSync(password, salt);
